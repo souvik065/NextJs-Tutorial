603ae7c7dcefcb3efac5780a026bdbc17f1eb076
souvik <bhattacharyasouvik065@gmail.com>
2023-06-09 23:27:41 +0530

    Next Js Beginner Pages and Project structure.

---
+++ next-app/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>NextJs First Post</title>
+      </Head>
+      <h1>First Blog Post</h1>
+    </>
+  );
+}
