4b85aadff04219b1d6c8ada820ea434ce8399747
souvik <bhattacharyasouvik065@gmail.com>
2023-06-12 20:49:48 +0530

    Learn to use Fetcher Function.

--- next-app/pages/posts/user.js
+++ next-app/pages/posts/user.js
@@ -1,6 +1,15 @@
 import useSWR from "swr";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+};
 
 export default function Users() {
   const { user, isLoading, isError } = getData();
@@ -46,7 +55,17 @@ export async function getStaticProps() {
 }
 
 function getData() {
-  const { data, error } = useSWR("http://localhost:3000/api/users", fetcher);
+  const { data, error } = useSWR("http://localhost:3000/api/users", fetcher, {
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      if (error.status === 404) return;
+
+      if (key === "/api/user") return;
+
+      if (retryCount >= 10) return;
+
+      setTimeout(() => revalidate({ retryCount }), 5000);
+    },
+  });
 
   return {
     user: data,
