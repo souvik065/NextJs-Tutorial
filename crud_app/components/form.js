1e594c976ef78efbb6c0b6ecbf30481ee4a7d62c
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-07-02 18:26:23 +0530

    Implemneted On Form Success and Erro Component

--- crud_app/components/form.js
+++ crud_app/components/form.js
@@ -1,6 +1,8 @@
 "use client";
 import { useReducer } from "react";
 import { BiPlus } from "react-icons/bi";
+import Success from "./success";
+import Bug from "./bug";
 
 const formReducer = (state, event) => {
   return {
@@ -13,8 +15,13 @@ export default function Form() {
   const [formData, setFormData] = useReducer(formReducer, {});
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (Object.keys(formData).length == 0)
+      return console.log("Don't have Form Data");
+
     console.log(formData);
   };
+
+  if (Object.keys(formData).length > 0) return <Bug message={"Error"}></Bug>;
 
   return (
     <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
