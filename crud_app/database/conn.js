5255c03a8e98544be679ad1d29ef553e105b7af4
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-07-06 23:19:21 +0530

    Created an Schema and Controler form Users

--- crud_app/database/conn.js
+++ crud_app/database/conn.js
@@ -1,10 +1,8 @@
-const MONGO_URI =
-  "mongodb+srv://admin:nextjscrud@nextjscrud.8risius.mongodb.net/?retryWrites=true&w=majority";
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGO_URI);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
 
     if (connection.readyState == 1) {
       console.log("Connected to MongoDB");
