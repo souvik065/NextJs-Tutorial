b11fb65f88f35a2850337deb28c98a67c1f074d9
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-07-07 23:00:10 +0530

    Make an file called helper.js to fetch data or consume API From backend and Fetch it to frontend.

--- crud_app/components/table.js
+++ crud_app/components/table.js
@@ -1,7 +1,9 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
 import data from "../database/data.json";
+import { getUser } from "../lib/helper";
 
 export default function Table() {
+  getUser().then((res) => console.log(res));
   return (
     <table className="min-w-full table-auto">
       <thead>
@@ -41,7 +43,11 @@ function Tr(props) {
   return (
     <tr className="bg-gray-50 text-center">
       <td className="px-16 py-2 flex flex-row items-center">
-        <img src={avatar || "#"} alt="" />
+        <img
+          src={avatar || "#"}
+          alt=""
+          className="h-8 w-8 rounded-full object-cover"
+        />
         <span className="text-center ml-2 font-semibold">
           {name || "Unkonwn"}
         </span>
