22517f482573347476271d5fb6c6da7902a56c2b
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-07-07 20:59:00 +0530

    Implented Dynamic API Endpoint

---
+++ crud_app/pages/api/users/[userId].js
@@ -0,0 +1,33 @@
+import connectMongo from "../../../database/conn";
+import {
+  getUsers,
+  postUser,
+  putUser,
+  deleteUser,
+  getUser,
+} from "../../../database/controller";
+
+export default function handler(req, res) {
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error Inside the connection" })
+  );
+
+  // type of request
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      getUser(req, res);
+      break;
+    case "PUT":
+      putUser(req, res);
+      break;
+    case "DELETE":
+      deleteUser(req, res);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
