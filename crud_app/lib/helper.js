71c2f59116f502af24d39a9938ac9f4a7a484068
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-07-08 17:37:59 +0530

    Created CRUD function for frontend into helper.js which will perform all the CRUD operations.

--- crud_app/lib/helper.js
+++ crud_app/lib/helper.js
@@ -1,8 +1,63 @@
 const BASE_URL = "http://localhost:3000/";
 
-export const getUser = async () => {
+//Fetching All Users
+export const getUsers = async () => {
   const res = await fetch(`${BASE_URL}/api/users`);
   const json = await res.json();
 
   return json;
 };
+
+// Fetching Single User by Id
+export const getUser = async (userId) => {
+  const response = await fetch(`${BASE_URL}api/users/${userId}`);
+  const json = await response.json();
+
+  if (json) return json;
+  return {};
+};
+
+// posting a new user
+export async function addUser(formData) {
+  try {
+    const Options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    };
+
+    const response = await fetch(`${BASE_URL}api/users`, Options);
+    const json = await response.json();
+
+    return json;
+  } catch (error) {
+    return error;
+  }
+}
+
+// update a new user
+export async function updateUser(userId, formData) {
+  const Options = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  };
+
+  const response = await fetch(`${BASE_URL}api/users/${userId}`, Options);
+  const json = await response.json();
+
+  return json;
+}
+
+//Delete a new User
+export async function deleteUser(userId) {
+  const Options = {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  const response = await fetch(`${BASE_URL}api/users/${userId}`, Options);
+  const json = await response.json();
+
+  return json;
+}
